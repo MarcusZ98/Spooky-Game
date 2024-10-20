fe55020de25670d61bc5dbe8de964bd89e1240d8
MarcusZ98 <marcus.zibung@gmail.com>
2024-10-20 22:12:02 +0200

    Added: CustomConfig.ini file & GameInstance reading file at boot

--- Source/SpookyGame/SG_GameInstance.cpp
+++ Source/SpookyGame/SG_GameInstance.cpp
@@ -3,3 +3,26 @@
 
 #include "SG_GameInstance.h"
 
+void USG_GameInstance::Init()
+{
+	Super::Init();
+	ReadIniFile();
+}
+
+void USG_GameInstance::ReadIniFile()
+{
+	FString Path = FPaths::ProjectConfigDir() + "CustomConfig.ini";
+
+	if(GConfig)
+	{
+		//Audio
+		GConfig->GetFloat(TEXT("Audio"), TEXT("MasterVolumeMultiplier"), MasterVolumeMultiplier, Path);
+		GConfig->GetFloat(TEXT("Audio"), TEXT("MusicVolumeMultiplier"), MusicVolumeMultiplier, Path);
+		GConfig->GetFloat(TEXT("Audio"), TEXT("SFXVolumeMultiplier"), SFXVolumeMultiplier, Path);
+		GConfig->GetFloat(TEXT("Audio"), TEXT("VoiceVolumeMultiplier"), VoiceVolumeMultiplier, Path);
+
+		//Controller
+		GConfig->GetFloat(TEXT("Controller"), TEXT("LookSpeedMultiplier"), LookSpeedMultiplier, Path);
+		GConfig->GetFloat(TEXT("Controller"), TEXT("WalkSpeedMultiplier"), WalkSpeedMultiplier, Path);
+	}
+}
